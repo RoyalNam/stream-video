bfdfc0e689c4125b7ba0bcc79bbb05625af6ded2
RoyalNam <hoangnam242003@gmail.com>
2024-01-21 16:40:10 +0700

    b

--- src/app/accounts/login/page.tsx
+++ src/app/accounts/login/page.tsx
@@ -1,34 +1,42 @@
 'use client';
 import React from 'react';
-import AccountLayout from '../layout';
 import LabelWithInput from '@/components/LabelWithInput';
 import Button from '@/components/Button';
 import Link from 'next/link';
 
 const Login = () => {
     return (
-        <AccountLayout title="Sign In">
-            <div className="w-[500px]">
-                <form action="" method="post">
-                    <div className="flex flex-col gap-3">
-                        <LabelWithInput title="Email" type="email" />
-                        <LabelWithInput title="Password" type="password" />
-                        <div>
-                            <input type="checkbox" name="Remember" id="" title="Remember" />
-                            <label className="ml-2">Remember Me</label>
-                        </div>
+        <div className="absolute top-0 bottom-0 left-0 right-0 bg-[url('/bg.jpeg')] z-50 w-full h-full">
+            <div className="flex flex-1 w-full h-full justify-center items-center">
+                <div className="bg-bgColor p-7 w-auto -mt-20">
+                    <div className="border-b mb-5 pb-2">
+                        <h4 className="text-3xl font-semibold uppercase text underline underline-offset-[13px] decoration-4 decoration-primary">
+                            Sign In
+                        </h4>
                     </div>
-                    <div className="my-4">
-                        <Button onClick={() => {}} title="Log In" />
+                    <div className="w-[500px]">
+                        <form action="" method="post">
+                            <div className="flex flex-col gap-3">
+                                <LabelWithInput title="Email" type="email" />
+                                <LabelWithInput title="Password" type="password" />
+                                <div>
+                                    <input type="checkbox" name="Remember" id="" title="Remember" />
+                                    <label className="ml-2">Remember Me</label>
+                                </div>
+                            </div>
+                            <div className="my-4">
+                                <Button onClick={() => {}} title="Log In" />
+                            </div>
+                            <div className="text-primary flex gap-2">
+                                <Link href={'/'}>Register</Link>
+                                <span className="text-white">|</span>
+                                <Link href={'/'}>Lost your password</Link>
+                            </div>
+                        </form>
                     </div>
-                    <div className="text-primary flex gap-2">
-                        <Link href={'/'}>Register</Link>
-                        <span className="text-white">|</span>
-                        <Link href={'/'}>Lost your password</Link>
-                    </div>
-                </form>
+                </div>
             </div>
-        </AccountLayout>
+        </div>
     );
 };
 
--- src/app/accounts/register/page.tsx
+++ src/app/accounts/register/page.tsx
@@ -1,30 +1,38 @@
 'use client';
 
 import React from 'react';
-import AccountLayout from '../layout';
 import LabelWithInput from '@/components/LabelWithInput';
 import Button from '@/components/Button';
 
 const Register = () => {
     return (
-        <AccountLayout title="Register">
-            <div className="w-[500px]">
-                <form action="" method="post">
-                    <div className="flex gap-3 flex-col">
-                        <div className="grid grid-cols-2 gap-4">
-                            <LabelWithInput title="First Name" />
-                            <LabelWithInput title="Last Name" />
-                        </div>
-                        <LabelWithInput title="Email" type="email" />
-                        <LabelWithInput title="Password" />
-                        <LabelWithInput title="Repeat Password" />
+        <div className="absolute top-0 bottom-0 left-0 right-0 bg-[url('/bg.jpeg')] z-50 w-full h-full">
+            <div className="flex flex-1 w-full h-full justify-center items-center">
+                <div className="bg-bgColor p-7 w-auto -mt-20">
+                    <div className="border-b mb-5 pb-2">
+                        <h4 className="text-3xl font-semibold uppercase text underline underline-offset-[13px] decoration-4 decoration-primary">
+                            Register
+                        </h4>
                     </div>
-                    <div className="mt-8 mb-2">
-                        <Button title="Register" onClick={() => {}} />
+                    <div className="w-[500px]">
+                        <form action="" method="post">
+                            <div className="flex gap-3 flex-col">
+                                <div className="grid grid-cols-2 gap-4">
+                                    <LabelWithInput title="First Name" />
+                                    <LabelWithInput title="Last Name" />
+                                </div>
+                                <LabelWithInput title="Email" type="email" />
+                                <LabelWithInput title="Password" />
+                                <LabelWithInput title="Repeat Password" />
+                            </div>
+                            <div className="mt-8 mb-2">
+                                <Button title="Register" onClick={() => {}} />
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
-        </AccountLayout>
+        </div>
     );
 };
 
