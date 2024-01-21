6d02da4825ebda263e6c6f6b353fac91a03313f0
RoyalNam <hoangnam242003@gmail.com>
2024-01-21 16:27:09 +0700

    fixx

--- src/app/accounts/layout.tsx
+++ src/app/accounts/layout.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-const AccountLayout = ({ children, title }: { children: React.ReactNode; title: string }) => {
+type AccountLayoutProps = {
+    children: ReactNode;
+    title: string;
+};
+
+const AccountLayout: React.FC<AccountLayoutProps> = ({ children, title }) => {
     return (
-        <div className="absolute top-0 bottom-0 left-0 right-0 bg-[url('/bg.jpeg')] z-30 w-full h-full">
+        <div className="absolute top-0 bottom-0 left-0 right-0 bg-[url('/bg.jpeg')] z-50 w-full h-full">
             <div className="flex flex-1 w-full h-full justify-center items-center">
                 <div className="bg-bgColor p-7 w-auto -mt-20">
                     <div className="border-b mb-5 pb-2">
